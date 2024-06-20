0aba7edfb28330ab9b5ca7b2428d72097386e13e
hiankarodrigues <hiankarodrigues@creago.org.br>
2024-06-20 16:23:47 -0300

    inserindo dinamicamente e em batch

--- src/services/stream.service.ts
+++ src/services/stream.service.ts
@@ -3,30 +3,36 @@ import * as fs from 'fs'
 import * as csvParser from 'csv-parser'
 import { PrismaService } from './prisma.service'
 import { Prisma } from '@prisma/client'
-import { bulk } from 'sql-template-tag'
+import { table } from 'console'
 
 const date = new Date()
+
 @Injectable()
 export class StreamService {
   constructor(private readonly prisma: PrismaService) {}
 
   async readCSV(path: string) {
-    // const tableName =
-    //   date.getMonth().toString() + '-' + date.getFullYear().toString() + '- CAR'
-    const batchSize = 200 // Tamanho do lote para inserção em massa
+    // nome da tabela com hifen da erro na hora de criar
+    const tableName =
+      date.getMonth().toString() + '_' + date.getFullYear().toString() + '_CAR'
+
+    const batchSize = 100 // Tamanho do lote para inserção em massa
     const batch = []
 
-    const tableName = 'propriety2'
     let contador = 0
 
+    // Checa se a tabela existe
     const result = await this.tableExists(tableName)
 
     if (!result) {
-      console.log('tabela nao existe')
+      console.log(
+        `Tabela ${tableName} nao existe, criando uma tabela com este nome...`,
+      )
 
-      this.createTable(tableName)
+      await this.createTable(tableName)
     }
 
+    // Lendo o CSV
     return new Promise<void>((resolve, reject) => {
       const stream = fs
         .createReadStream(path)
@@ -52,7 +58,7 @@ export class StreamService {
           stream.pause()
           await this.insertData(tableName, batch)
 
-          batch.length = 0 // Limpa o lote após a inserção
+          batch.length = 0
           console.log('Linhas lidas:', contador)
           stream.resume()
         }
@@ -62,12 +68,12 @@ export class StreamService {
         if (batch.length > 0) {
           try {
             await this.insertData(tableName, batch)
+            console.log('Linhas lidas:', contador)
           } catch (error) {
             console.error('Error creating final propriety batch:', error)
             reject(error)
           }
         }
-        // console.log('CSV lido, contador: ', contador)
         resolve()
       })
 
@@ -80,6 +86,19 @@ export class StreamService {
 
   async createTable(tableName: string) {
     try {
+      await this.prisma.$executeRawUnsafe(`
+            CREATE TABLE ${tableName} (
+                cod_imovel VARCHAR(255) PRIMARY KEY,
+                mod_fiscal FLOAT,
+                num_area FLOAT,
+                municipio VARCHAR(255),
+                nome_propr VARCHAR(255),
+                cpf_cnpj VARCHAR(255),
+                proprietar VARCHAR(255),
+                situacao VARCHAR(255),
+                shape_leng FLOAT,
+                shape_area FLOAT
+            );`)
       console.log(`Tabela ${tableName} criada com sucesso.`)
     } catch (error) {
       console.error('Erro ao criar a tabela "propriety":', error)
@@ -91,8 +110,7 @@ export class StreamService {
     const result = await this.prisma.$queryRaw`
     SELECT COUNT(*) as table_exists
     FROM information_schema.tables
-    WHERE table_schema = 'car' AND table_name = ${tableName};
-`
+    WHERE table_schema = 'car' AND table_name = ${tableName};`
 
     console.log(result[0].table_exists > 0)
 
@@ -101,15 +119,17 @@ export class StreamService {
 
   async insertData(tableName: string, batch) {
     try {
-      const res = await // .$executeRawUnsafe(`${query}, ${dataTeste}`)
-      this.prisma.$executeRaw`
-        INSERT INTO propriety
+      const res = await this.prisma.$executeRaw`
+        INSERT IGNORE INTO ${Prisma.raw(`\`${tableName}\``)}
         ( cod_imovel, mod_fiscal,num_area, municipio,nome_propr,cpf_cnpj,proprietar, situacao, shape_leng, shape_area)
         VALUES ${Prisma.join(
           batch.map((row) => Prisma.sql`(${Prisma.join(row)})`),
-        )};`
+        )}
+        ;`
 
       console.log('retorno: ', res)
-    } catch (error) {}
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
